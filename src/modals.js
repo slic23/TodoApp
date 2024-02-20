f344438e67ca1de80b5ed3356950f9d43adc3640
slic23 <socrates98@protonmail.com>
2024-02-20 13:18:08 +0100

    change made

--- src/modals.js
+++ src/modals.js
@@ -8,6 +8,35 @@ function MakingInputs(){
           let modal = document.querySelector(".modal") 
           modal.style.display ="none"
         })
+
+        document.getElementById('buton').addEventListener('click',() => {
+          console.log("Hola")
+        
+         document.body.querySelector('.modal2').style.display = "grid"
+         
+          document.querySelector('.container').style.opacity = 0.1; 
+        })
+        
+        
+        document.body.querySelector('.pointer').addEventListener('click', () => {
+          document.body.querySelector('.modal2').style.display = "none"
+          document.querySelector(".container").style.opacity = 1
+          
+        })
+
+
+      document.querySelector('.saving').addEventListener('click',()=>{
+        // control errors first , just testing avoid repeting code 
+        //
+        
+        createTodo()
+        document.querySelector('.modal2').style.display ="none"
+        document.querySelector('.container').style.opacity = 1
+      })
+
+
+      
+
 }
 
 function select( ){
@@ -18,10 +47,33 @@ function select( ){
     novaOption.text = document.getElementById('ProjecName').value
     console.log(novaOption)
     select.add(novaOption)
+    
+  })
+
+  document.querySelector('select').addEventListener('change',(event)=>{
+  document.querySelector('.changeIt').innerText = event.target.value
   })
 }
+
+function createTodo(){
+/// Dom title 
+  let container = document.createElement('div')
+  container.setAttribute("id","subcontainer")
+  document.getElementById('todosWANTtoAdd').append(container)
+  let Domtitle = document.createElement('div')
+   container.append(Domtitle)
+   Domtitle.innerText = document.querySelector(".titulo").value
+   
+// Dom date 
+ let  Domdate = document.createElement('div')
+  container.append(Domdate)
+ Domdate.innerText = document.querySelector(".fecha").value
+
+}
+
+
 export {
-    MakingInputs,select
+    MakingInputs,select 
 }
 
 
